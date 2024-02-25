698f697fd8835558480025a8f30e91969d4fb445
CristiBCN <cristidnc72@gmail.com>
2024-02-25 17:01:00 +0100

    Created and displayed the squares
    The code creates  and displays 16x16 squares regardless the container dimensions

--- logic.js
+++ logic.js
@@ -1,5 +1,11 @@
 let container = document.querySelector('#container');
 
+let width = container.clientWidth / 16;
+let height = container.clientHeight / 16;
+
+console.log(width + " x " + height);
+
+
 function createElem(type, cls, text, container) {
   let el = document.createElement(type);
   //el.setAttribute('class', cls);
@@ -10,5 +16,15 @@ function createElem(type, cls, text, container) {
   return el;
 }
 
+for (let i = 0; i < 16 * 16; i++) {
 let square = createElem('div', 'square', '', container);
+square.style.cssText = `width: ${width}px; height: ${height}px`;
 
+square.addEventListener("mouseover", function() {
+  square.style.backgroundColor = "aqua";
+});
+
+square.addEventListener("mouseout", function() {
+  square.style.backgroundColor = "aqua";
+});
+}
