a35534ea79a265a95f01a19ddc5bd0199444d99e
CristiBCN <cristidnc72@gmail.com>
2024-02-27 21:13:05 +0100

    Task "Extra Credit 2" completed
    Now, the squares have a progressive darkening effect where each interaction adds 10% more black to the square.  We get a completely black square only after ten interactions.

--- logic.js
+++ logic.js
@@ -6,14 +6,14 @@ btn.addEventListener('click', function getNumber() {
   let n = +prompt('How many squares do you want to see on each direction? (Between 0 up to 50)', 16);
   if (n < 0 || n > 50) {
     return getNumber();
-  } else {console.log(n);
+  } else {
       createSquares(n);
   };  
 })
 
-function createElem(type, cls, text, container) {
+function createElem(type, id, cls, text, container) {
   let el = document.createElement(type);
-  //el.setAttribute('class', cls);
+  el.setAttribute('id', id);
   el.className = cls;
   el.textContent = text;
   container.append(el);
@@ -30,8 +30,8 @@ function createSquares(nr) {
   console.log(container.clientWidth + " x " + container.clientHeight);
 
   for (let i = 0; i < nr * nr; i++) {
-    let square = createElem('div', 'square', '', container);
-    square.style.cssText = `width: ${width}px; height: ${height}px`;
+    let square = createElem('div', i + 1, 'square', '', container);
+    square.style.cssText = `width: ${width}px; height: ${height}px;`;
     if (i < nr) {
       square.textContent = i + 1;      
     } else {
@@ -40,16 +40,28 @@ function createSquares(nr) {
       }      
     };
 
+    
+    square.mouseoverCount = 0;
+    
     let bkColor = getColor();
+    console.log('bkColor:', bkColor);
+    
     square.addEventListener("mouseover", function() {
-      square.style.backgroundColor = bkColor;
-    });
-
-    square.addEventListener("mouseout", function() {
-      square.style.backgroundColor = bkColor;
+      
+      square.mouseoverCount++;
+      let darkenPercent = square.mouseoverCount * 10;
+      let newColor = darkenColor(bkColor, darkenPercent);
+      console.log('newColor:', newColor);
+      square.style.backgroundColor = newColor;      
+      console.log(`Square ${event.target.id} has been moused over ${square.mouseoverCount} times.`);      
+      console.log(square.style.backgroundColor);
     });
   };
 };
+
+function getRandom(upTo) {
+  return Math.floor(Math.random() * upTo)
+}
 
 function getColor() {
   let R, G, B, A;
@@ -60,7 +72,15 @@ function getColor() {
   return `rgba(${R} ${G} ${B} / ${A}%)`
 }
 
-function getRandom(upTo) {
-  return Math.floor(Math.random() * upTo)
-}
+function darkenColor(color, percent) {
+  let rgba = color.slice(5, -1).split(' ');  
+  
+  for (let i = 0; i < 3; i++) {
+    rgba[i] = Math.floor(rgba[i] * (1 - percent / 100));
+  };
 
+  let val = 100 - parseFloat(rgba[4]);  
+  rgba[4] = (parseFloat(rgba[4]) + (val / 100) * percent);
+  
+  return `rgba(${rgba[0]} ${rgba[1]} ${rgba[2]} / ${rgba[4]}%)`;
+}
